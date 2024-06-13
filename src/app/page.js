f6e0e863ca7804873dec27dd2cd7a3363e5529d8
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-13 23:52:32 +0200

    some things done?

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,19 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full flex-row items-center justify-center font-mono text-sm lg:flex">
-        <p className="m-5">
-          Hi
-        </p>
-        <Image 
-          src="/cat2.gif"
-          alt="kitten!!11!"
-          width={32}
-          height={32}
-        />
+      <div className="z-10 max-w-5xl w-full flex-row items-center justify-center align-middle font-mono text-sm lg:flex">
+        <div className="flex justify-center">
+          <p className="m-3">
+            Hi
+          </p>
+          <Image
+            className=" self-center"
+            src="/cat2.gif"
+            alt="kitten!!11!"
+            width={32}
+            height={32}
+          />
+        </div>
       </div>
     </main>
   );
