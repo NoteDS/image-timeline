fd8656af06bf89ee202e4a4e899deb4d2b7cf828
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-13 22:53:31 +0200

    some changes

--- README.md
+++ README.md
@@ -4,12 +4,11 @@
 
 ## About
 Uhmmmmmm... just a small photos project I guess...
+If you have any ideas for the name please send us (this project has the most generic ahhh name I've ever seen)
 Basically a image reviewer from specific dates. Built using Next.js and tailwind. It will probably have a good design (if we feel like doing it) but yeah thats it.
 
 ## Recommendations
-Installing a tailwind intelisense plugin (VSCode)
-Actually using the docs (ik its hard but we must)
-Writing good code (also hard but less hard but still hardw)
+Installing a tailwind intelisense plugin (VSCode). Actually using the docs (ik its hard but we must use them). Writing good code (also hard but less hard but still hard)
 
 ## Docs
 [tailwindcss docs](https://tailwindcss.com/docs/)
@@ -23,3 +22,11 @@ Running the dev server (very basic)
 ```bash
 npm run dev
 ```
+Add something here if its gonna be useful later.
+
+
+## Things we need
+- better icon
+- better name
+- better code
+- better developers
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image"; // for now we are keeping this image
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          The page content for now...
+      <div className="z-10 max-w-5xl w-full flex-col items-center justify-center font-mono text-sm lg:flex">
+        <p className="fixed m-5 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Home
         </p>
+        The page content for now... (gonna fix that later tho)
       </div>
     </main>
   );
